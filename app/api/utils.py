3915b7857376bc2773c9e33f279fd88605be487d
Ali Hashempour <ali.hashempour@gmail.com>
2023-06-01 23:05:16 +0330

    feat: insert url to db

--- app/api/utils.py
+++ app/api/utils.py
@@ -5,7 +5,7 @@ def db_connection():
     host = 'localhost'
     database = 'url-shortener'
     user = 'postgres'
-    password = 22442883
+    password = '22442883'
 
     # Establish the connection
     connection = psycopg2.connect(
@@ -17,4 +17,16 @@ def db_connection():
     return connection
 
 
+def db_insert(request_url):
+    url_id = abs(hash(request_url)) % (10 ** 8)
 
+    con = db_connection()
+    cursor = con.cursor()
+    val = (url_id, request_url)
+    insert_query = """
+        INSERT INTO urls_table (ID, URL_ADDRESS)
+        VALUES (%s, %s)
+    """
+    cursor.execute(insert_query, val)
+    con.commit()
+    return url_id
