4008ce1b4e64baa017b3a0deff0a0fa62d3ed8aa
Ali Hashempour <ali.hashempour@gmail.com>
2023-06-01 23:35:57 +0330

    feat: shortened url route handler

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
 from flask_cors import CORS
 import app.api.utils as utils
 
@@ -13,3 +13,11 @@ def handler():
     _id = utils.db_insert(request_url=requested_url)
     new_url = {"new_url": f"localhost:5000/api/V1/{_id}"}
     return new_url
+
+
+@app.route('/api/V1/<url_id>')
+def shortened_url_handler(url_id):
+    _id = url_id
+    actual_url = utils.db_select(url_id=_id)
+    return redirect(f"https://{actual_url}")
+
