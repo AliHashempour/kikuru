29f0bdcd45f2ec763966909babe7a3975cb69f4e
Ali Hashempour <ali.hashempour@gmail.com>
2023-06-01 23:05:54 +0330

    feat: new url body return

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -9,4 +9,7 @@ CORS(app)
 @app.route('/api/V1/url_request', methods=['post'])
 def handler():
     request_body = request.json
-    return request_body
+    requested_url = request_body['url']
+    _id = utils.db_insert(request_url=requested_url)
+    new_url = {"new_url": f"localhost:5000/api/V1/{_id}"}
+    return new_url
